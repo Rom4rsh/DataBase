91d200ae414bfb9de0dcf5ff3331d20f90d9d571
romarsh <rom4rsh@gmail.com>
2025-05-01 01:25:29 +0300

    HW

--- src/main/resources/liquibase/sql/lesson-three.sql
+++ src/main/resources/liquibase/sql/lesson-three.sql
@@ -1,7 +1,7 @@
--- liquibase formated sql
+--liquibase formated sql
 
--- changeset romarsh:1
+--changeset romarsh:1
 CREATE INDEX student_name_index ON student(name);
 
--- changeset romarsh:2
+--changeset romarsh:2
 CREATE INDEX faculty_name_index ON faculty(name);
