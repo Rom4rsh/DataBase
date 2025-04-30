7430832915d13ae1589e4a6f838fafe1cb24ffb0
romarsh <rom4rsh@gmail.com>
2025-04-30 17:52:00 +0300

    HW

--- src/scripts423.sql
+++ src/scripts423.sql
@@ -7,3 +7,9 @@ INNER JOIN faculty ON student.faculty_id = faculty.id;
 SELECT SELECT student.name,student.age
 FROM student
 INNER JOIN avatar ON student.id = avatar.student_id;
+
+
+<dependency>
+    <groupId>org.liquibase</groupId>
+    <artifactId>liquibase-core</artifactId>
+</dependency>
