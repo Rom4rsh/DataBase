6110bc6c97639b143aee0f70770cd38e5057ec8d
romarsh <rom4rsh@gmail.com>
2025-05-05 16:09:59 +0300

    HW

--- src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.service;
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,7 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 public class AvatarServiceImpl implements AvatarService {
     private final AvatarRepository avatarRepository;
     private final StudentRepository studentRepository;
+    private static final Logger logger = LoggerFactory.getLogger(AvatarServiceImpl.class);
 
     @Value("${path.to.avatars.folder}")
     private String avatarsDir;
@@ -38,20 +41,28 @@ public class AvatarServiceImpl implements AvatarService {
 
 
     public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
-        Student student = studentRepository.getById(studentId);
+        logger.info("Was invoked method for upload avatar for student id = {}", studentId);
 
+        Student student = studentRepository.getById(studentId);
         String extension = getExtensions(avatarFile.getOriginalFilename());
         Path filePath = Path.of(avatarsDir, student.getId() + "." + extension);
+
+        logger.debug("Resolved file path: {}", filePath);
 
         Files.createDirectories(filePath.getParent());
         Files.deleteIfExists(filePath);
 
-        try (InputStream is = avatarFile.getInputStream(); OutputStream os = Files.newOutputStream(filePath, StandardOpenOption.CREATE_NEW); BufferedInputStream bis = new BufferedInputStream(is, 1024); BufferedOutputStream bos = new BufferedOutputStream(os, 1024)) {
+        try (InputStream is = avatarFile.getInputStream();
+             OutputStream os = Files.newOutputStream(filePath, StandardOpenOption.CREATE_NEW);
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+             BufferedOutputStream bos = new BufferedOutputStream(os, 1024)) {
             bis.transferTo(bos);
+            logger.debug("Avatar file was written to {}",filePath);
         }
 
         Avatar avatar = avatarRepository.findByStudentId(studentId).orElse(null);
         if (avatar == null) {
+            logger.warn("No existing avatar found for studentId = {}, creating new", studentId);
             avatar = new Avatar();
             avatar.setStudent(student);
         }
@@ -59,15 +70,18 @@ public class AvatarServiceImpl implements AvatarService {
         avatar.setFilePath(filePath.toString());
         avatar.setFileSize(avatarFile.getSize());
         avatar.setMediaType(avatarFile.getContentType());
-        avatar.setData(generateDataForDB(filePath)); // 6. Сжатое изображение в БД
+        avatar.setData(generateDataForDB(filePath)); // Сжатое изображение в БД
         avatarRepository.save(avatar);
+        logger.info("Avatar for studentID = {} saved",studentId);
     }
 
 
     private byte[] generateDataForDB(Path filePath) throws IOException {
+        logger.debug("generating preview image for path: {}",filePath);
         try (InputStream is = Files.newInputStream(filePath); BufferedInputStream bis = new BufferedInputStream(is, 1024); ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             BufferedImage image = ImageIO.read(bis);
             if (image == null) {
+                logger.error("Invalid image file at {}",filePath);
                 throw new IOException("Файл не является изображением или поврежден");
             }
 
@@ -78,16 +92,19 @@ public class AvatarServiceImpl implements AvatarService {
             graphics2D.dispose();
 
             ImageIO.write(preview, getExtensions(filePath.getFileName().toString()), baos);
+            logger.debug("Preview image generated successfully for {}",filePath);
             return baos.toByteArray();
         }
 
     }
 
     public Avatar findAvatar(Long studentID) {
+        logger.info("Was invoked method for find avatar with student id = {}",studentID);
         return avatarRepository.findByStudentId(studentID).orElse(new Avatar());
     }
 
     private String getExtensions(String fileName) {
+        logger.debug("Was invoked method get extensions with file name: {}",fileName);
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -11,6 +13,7 @@ import java.util.*;
 @Service
 public class StudentServiceImpl implements StudentService {
     private final StudentRepository studentRepository;
+    private static final Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     @Autowired
     public StudentServiceImpl(StudentRepository studentRepository) {
@@ -18,53 +21,64 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
+
     public Student createStudent(Student student) {
+        logger.info("Was invoked method create student");
         return studentRepository.save(student);
     }
 
     @Override
     public Collection<Student> findByAgeBetween(int min, int max) {
+        logger.debug("Was invoked method find by age min = {} between max = {}",min,max);
         return studentRepository.findByAgeBetween(min, max);
     }
 
     @Override
     public Faculty getFacultyByStudent(Long studentId) {
+        logger.debug("Was invoked method get faculty by student id = {}",studentId);
         return studentRepository.findFacultyByStudentId(studentId);
     }
 
     @Override
     public Student findStudent(long id) {
+        logger.warn("Was invoked method find student by id = {}",id);
         return studentRepository.findById(id).orElse(null);
     }
 
     @Override
     public void deleteStudents(long id) {
+        logger.info("Was invoked method find student by id = {}",id);
         studentRepository.deleteById(id);
     }
 
     @Override
     public Student editStudent(long id, Student student) {
+        logger.info("Was invoked method edit student with id = {}",id);
         student.setId(id);
         return studentRepository.save(student);
     }
 
     @Override
     public Collection<Student> getAllStudents() {
+        logger.debug("Was invoked method get all student" );
         return studentRepository.findAll();
     }
 
     @Override
     public Long getStudentCount() {
+        logger.debug("Was invoked method get student count");
         return studentRepository.getStudentCount();
     }
 
     @Override
     public Long getStudentAvgAge() {
+        logger.debug("Was invoked method get student avg age ");
         return studentRepository.getStudentAvgAge();
     }
 
     @Override
     public List<Student> getFiveLastStudent() {
+        logger.debug("Was invoked method get five last student ");
         return studentRepository.getFiveLastStudent();
     }
 }
